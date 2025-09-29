223ca9fadae235eee57f9b94c49158400b2af895
hadin <automation22@mis.prangroup.com>
2025-09-29 16:57:03 +0600

    modify for only pip

--- Worker.cs
+++ Worker.cs
@@ -20,20 +20,20 @@ namespace AllSoftwareTracker
             if (Directory.Exists(@"D:\"))
             {
                 fileName = @"D:\" + "AllTracker_LogFile.txt";
-                existingFileHIP = @"D:\" + "HIP_LogFile.txt";
-                existingFilePIP = @"D:\" + "HIP_LogFile.txt";
+                //existingFileHIP = @"D:\" + "HIP_LogFile.txt";
+                existingFilePIP = @"D:\" + "PIP_LogFile.txt";
             }
             else if (Directory.Exists(@"E:\"))
             {
                 fileName = @"E:\" + "AllTracker_LogFile.txt";
-                existingFileHIP = @"E:\" + "HIP_LogFile.txt";
-                existingFilePIP = @"E:\" + "HIP_LogFile.txt";
+                //existingFileHIP = @"E:\" + "HIP_LogFile.txt";
+                existingFilePIP = @"E:\" + "PIP_LogFile.txt";
             }
             else
             {
                 fileName = Path.Combine(Directory.GetCurrentDirectory(), "AllTracker_LogFile.txt");
-                existingFileHIP = Path.Combine(Directory.GetCurrentDirectory(), "HIP_LogFile.txt");
-                existingFilePIP = Path.Combine(Directory.GetCurrentDirectory(), "HIP_LogFile.txt");
+                //existingFileHIP = Path.Combine(Directory.GetCurrentDirectory(), "HIP_LogFile.txt");
+                existingFilePIP = Path.Combine(Directory.GetCurrentDirectory(), "PIP_LogFile.txt");
             }
             if (!File.Exists(fileName))
             {
@@ -45,77 +45,52 @@ namespace AllSoftwareTracker
             File.AppendAllText(fileName, "Start at " + DateTime.Now.ToString() + "\n");
             while (!stoppingToken.IsCancellationRequested)
             {
-                string processName1 = "SmartManufacturingLocal_PIP_RIP_DIP_PLC";
-                var processes1 = Process.GetProcessesByName(processName1);
+                //string processName1 = "SmartManufacturingLocal_PIP_RIP_DIP_PLC";
+                //var processes1 = Process.GetProcessesByName(processName1);
 
-                string processName2 = "SmartManufacturingLocal_HIP_PLC";
-                var processes2 = Process.GetProcessesByName(processName2);
-                if (processes1.Length == 0)
-                {
-                    File.AppendAllText(fileName, "\nProcess not running. " + DateTime.Now.ToString());
-                    await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_PIP_RIP_DIP_PLC is not running!");
-                }
-                else
-                {
-                    foreach (var process in processes1)
-                    {
-                        if (!process.Responding)
-                        {
-                            File.AppendAllText(fileName, "\nProcess not responding. " + DateTime.Now.ToString());
-                            await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_PIP_RIP_DIP_PLC is not running!");
-                        }
-                        else
-                        {
-                            File.AppendAllText(fileName, "\nProcess is running fine. " + DateTime.Now.ToString());
-                        }
-                    }
-                }
+                //string processName2 = "SmartManufacturingLocal_HIP_PLC";
+                //var processes2 = Process.GetProcessesByName(processName2);
+                //if (processes1.Length == 0)
+                //{
+                //    File.AppendAllText(fileName, "\nProcess not running. " + DateTime.Now.ToString());
+                //    await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_PIP_RIP_DIP_PLC is not running!");
+                //}
+                //else
+                //{
+                //    foreach (var process in processes1)
+                //    {
+                //        if (!process.Responding)
+                //        {
+                //            File.AppendAllText(fileName, "\nProcess not responding. " + DateTime.Now.ToString());
+                //            await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_PIP_RIP_DIP_PLC is not running!");
+                //        }
+                //        else
+                //        {
+                //            File.AppendAllText(fileName, "\nProcess is running fine. " + DateTime.Now.ToString());
+                //        }
+                //    }
+                //}
 
-                if (processes2.Length == 0)
-                {
-                    File.AppendAllText(fileName, "\nProcess not running. " + DateTime.Now.ToString());
-                    await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_HIP_PLC is not running!");
-                }
-                else
-                {
-                    foreach (var process in processes2)
-                    {
-                        if (!process.Responding)
-                        {
-                            File.AppendAllText(fileName, "\nProcess not responding. " + DateTime.Now.ToString());
-                            await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_HIP_PLC is not running!");
-                        }
-                        else
-                        {
-                            File.AppendAllText(fileName, "\nSmartManufacturingLocal_HIP_PLC is running fine. " + DateTime.Now.ToString());
-                        }
-                    }
-                }
-
-
-                #region FOR HIP
-                if (File.Exists(existingFileHIP))
-                {
-                    string? lastLine = "";
-                    lastLine = File.ReadLines(existingFileHIP).LastOrDefault();
-                    if (!string.IsNullOrWhiteSpace(lastLine) && DateTime.TryParse(lastLine, out DateTime recordedDate))
-                    {
-                        if ((DateTime.Now - recordedDate).TotalMinutes > 10)
-                        {
-                            await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_HIP is not running!");
-                        }
-                    }
-                    else
-                    {
-                        await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_HIP is under exception!");
-                    }
-                }
-                else
-                {
-                    await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_HIP's file does not find!");
-                }
-                #endregion
-
+                //if (processes2.Length == 0)
+                //{
+                //    File.AppendAllText(fileName, "\nProcess not running. " + DateTime.Now.ToString());
+                //    await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_HIP_PLC is not running!");
+                //}
+                //else
+                //{
+                //    foreach (var process in processes2)
+                //    {
+                //        if (!process.Responding)
+                //        {
+                //            File.AppendAllText(fileName, "\nProcess not responding. " + DateTime.Now.ToString());
+                //            await SendWhatsAppViaCallMeBot("SmartManufacturingLocal_HIP_PLC is not running!");
+                //        }
+                //        else
+                //        {
+                //            File.AppendAllText(fileName, "\nSmartManufacturingLocal_HIP_PLC is running fine. " + DateTime.Now.ToString());
+                //        }
+                //    }
+                //}
                 #region FOR PIP
                 if (File.Exists(existingFilePIP))
                 {
